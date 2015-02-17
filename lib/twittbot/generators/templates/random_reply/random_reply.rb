d4586c9f4307ed5293137c7c4189817ab4e2d9c3
nilsding <nilsding@nilsding.org>
2015-02-17 19:07:13 +0100

    added TemplateGenerator and example random-reply template

---
+++ lib/twittbot/generators/templates/random_reply/random_reply.rb
@@ -0,0 +1,14 @@
+<%
+  botpart_config = {
+    replies: [
+      "Yes.",
+      "No.",
+      "Absolutely.",
+      "I don't think so.",
+      "Maybe.",
+    ]
+  }
+%>
+Twittbot::BotPart.new :<%= @template_name %> do
+  puts config[:replies].sample
+end
---
+++ lib/twittbot/generators/templates/template_generator.rb
@@ -0,0 +1,65 @@
+require 'fileutils'
+require 'erubis'
+require 'yaml'
+
+module Twittbot
+  module Generators
+    class TemplateGenerator
+      include Thor::Shell
+
+      def initialize(template_name, options)
+        @template_name = template_name.gsub('-', '_')
+        @options = {
+            'template_options' => {}
+        }.merge!(options)
+        @options['template_dir'] = File.expand_path "../#{@template_name}", __FILE__
+      end
+
+      def create
+        files = Dir["#{@options['template_dir']}/**/*"]
+        files.each do |file|
+          real_filename = "lib/#{file.sub(/^#{@options['template_dir']}\//, '').sub(/^_/, '.')}"
+          real_path = File.expand_path "./#{real_filename}"
+
+          botpart_config = {}
+          erb = Erubis::Eruby.new File.read(file)
+          final_result = erb.result(binding)
+
+          if File.exist? real_path
+            say_status :exists, real_filename, :red
+          else
+            say_status :create, real_filename, :green
+            if File.directory? file
+              FileUtils.mkdir_p real_path
+            else
+              File.open real_path, 'w' do |f|
+                f.write final_result
+              end
+            end
+          end
+
+          save_config botpart_config, real_filename unless botpart_config.empty?
+        end
+      end
+
+      private
+
+      def save_config(config, path)
+        path = "etc/#{path.sub(/^lib\//, '').sub(/\.rb$/, '.yml')}"
+        real_path = File.expand_path "./#{path}"
+
+        if File.exist? real_path
+          say_status :merge, path, :yellow
+          existing_config = YAML.load_file real_path
+          config.merge! existing_config
+        else
+          say_status :create, path, :green
+        end
+
+        File.open real_path, 'w' do |f|
+          f.write config.to_yaml
+        end
+      end
+    end
+  end
+end
