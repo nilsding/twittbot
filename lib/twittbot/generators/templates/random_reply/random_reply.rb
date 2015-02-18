5aa9f5a85c462e3bc9d0b921425edac52bfbf42b
nilsding <nilsding@nilsding.org>
2015-02-18 16:28:03 +0100

    added a comment to botpart_config in the template

--- lib/twittbot/generators/templates/random_reply/random_reply.rb
+++ lib/twittbot/generators/templates/random_reply/random_reply.rb
@@ -1,4 +1,6 @@
 <%
+  # This is the config for this botpart.
+  # It is as a hash and will be serialized to YAML, and then stored in 'etc/template_name.yml'.
   botpart_config = {
     replies: [
       "Yes.",
