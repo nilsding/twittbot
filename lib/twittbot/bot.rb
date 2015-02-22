3d4aa763fb3e131a8f08a65fd972e5fb68f199c6
nilsding <nilsding@nilsding.org>
2015-02-22 17:19:46 +0100

    added opts hash

--- lib/twittbot/bot.rb
+++ lib/twittbot/bot.rb
@@ -102,19 +102,22 @@ module Twittbot
       end
     end
 
-    def handle_stream_object(object, _type)
+    def handle_stream_object(object, type)
+      opts = {
+          stream_type: type
+      }
       case object
         when Twitter::Streaming::FriendList
           do_callbacks :friend_list, object
         when Twitter::Tweet
           is_mention = (object.user.screen_name != $bot[:config][:screen_name] and object.text.include?("@" + $bot[:config][:screen_name]) and not object.retweet?)
-          do_callbacks :retweet, object if object.retweet? and object.retweeted_tweet.user.screen_name == $bot[:config][:screen_name]
-          do_callbacks :mention, object if is_mention
-          do_callbacks :tweet, object, mention: is_mention, retweet: object.retweet?
+          do_callbacks :retweet, object, opts if object.retweet? and object.retweeted_tweet.user.screen_name == $bot[:config][:screen_name]
+          do_callbacks :mention, object, opts if is_mention
+          do_callbacks :tweet, object, opts.merge({ mention: is_mention, retweet: object.retweet? })
         when Twitter::Streaming::Event
           case object.name
             when :favorite
-              do_callbacks :favorite, object
+              do_callbacks :favorite, object, opts
             else
               puts "no handler for #{object.class.to_s}/#{object.name}\n  -- object data:"
               require 'pp'
