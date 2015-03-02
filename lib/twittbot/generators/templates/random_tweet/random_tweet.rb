1f2a8611a3a3dea224521e1172f4bd40784c2f95
nilsding <nilsding@nilsding.org>
2015-03-02 20:20:15 +0100

    added an example cmd to random_tweet

--- lib/twittbot/generators/templates/random_tweet/random_tweet.rb
+++ lib/twittbot/generators/templates/random_tweet/random_tweet.rb
@@ -17,4 +17,20 @@ Twittbot::BotPart.new :<%= @template_name %> do
     # ... tweet a random entry of the :tweets list that you configured
     bot.tweet @config[:tweets].sample
   end
+
+  ## Uncomment the next few lines if you want to add new tweets to the tweet
+  ## list on the fly using direct messages.
+  ##
+  ## Example usage:
+  ## !atwt Tweet text
+  #cmd :atwt do |args|
+  #  # adds args (usually the tweet text) to the list of random tweets unless
+  #  # args already is in the list
+  #  @config[:tweets] << args unless @config[:tweets].include? args
+  #end
+  #
+  ## Auto-save!  \o/
+  #every 1, :hour do
+  #  save_config
+  #end
 end
