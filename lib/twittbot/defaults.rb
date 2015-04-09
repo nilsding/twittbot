ccb1d78b331e5281e3247f66572171b4a2b55d0c
Georg G <nilsding@nilsding.org>
2015-04-09 09:02:17 +0200

    0.2.0

--- lib/twittbot/defaults.rb
+++ lib/twittbot/defaults.rb
@@ -1,6 +1,6 @@
 module Twittbot
   # The version of Twittbot.
-  VERSION = "0.1.2"
+  VERSION = "0.2.0"
 
   CONSUMER_KEY = 'FYRuQcDbPAXAyVjuPZMuw' # :nodoc:
   CONSUMER_SECRET = 'KiLCYTftPdxNebl5DNcj7Ey2Y8YVZu7hfqiFRYkcg' # :nodoc:
