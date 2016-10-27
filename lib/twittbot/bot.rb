14fc5ddc512e0eef13745024fae947e598c6cc7c
Georg G <nilsding@nilsding.org>
2016-10-27 08:39:46 +0200

    bugfuchs

--- lib/twittbot/bot.rb
+++ lib/twittbot/bot.rb
@@ -209,7 +209,7 @@ module Twittbot
         when Twitter::DirectMessage
           do_direct_message object, opts
         when Twitter::Streaming::DeletedTweet
-          do_callbacks :deleted, object
+          do_callbacks :deleted, object, opts
         else
           puts "no handler for #{object.class.to_s}\n  -- object data:"
           require 'pp'
